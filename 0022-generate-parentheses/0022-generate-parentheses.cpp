561167d93488a978818a731b8fa9013fba3ec901
Karthik K <157501524+karthik-7777777@users.noreply.github.com>
2026-05-06 00:15:53 +0530

    Time: 2 ms (81.24%), Space: 16.3 MB (17.78%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-void par(int n,int op,int cp,string str,vector<string>&ans){
-    if(str.size()==2*n){
-        ans.push_back(str);
+void rec(int n,int l,int r,string s,vector<string>& ans){
+    if(r>n || l>n){
         return;
     }
-    if(op<n){
-        par(n,op+1,cp,str+"(",ans);
+    if(l==r && (l+r)/2 == n){
+        ans.push_back(s);
+        return;
     }
-    if(cp<op){
-        par(n,op,cp+1,str+")",ans);
+    rec(n,l+1,r,s+'(',ans);
+    if(l>r){
+        rec(n,l,r+1,s+')',ans);
     }
 }
     vector<string> generateParenthesis(int n) {
         vector<string>ans;
-        string str;
-        par(n,0,0,"",ans);
+        string s="";
+        rec(n,0,0,s,ans);
         return ans;
     }
 };
