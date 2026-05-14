3bce3292948b53b3593a7db525fa3ec44003a162
Karthik K <157501524+karthik-7777777@users.noreply.github.com>
2026-05-14 21:26:35 +0530

    Time: 3 ms (22.38%), Space: 8.2 MB (61.47%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 3857-minimum-cost-to-split-into-ones/3857-minimum-cost-to-split-into-ones.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minCost(int n) {
+        int m=n-1;
+        return (m*(m+1))/2;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
