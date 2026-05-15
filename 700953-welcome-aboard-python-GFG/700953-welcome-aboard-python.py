83e3865b654526aead627077f1156756614adb6a
Karthik K <157501524+karthik-7777777@users.noreply.github.com>
2026-05-15 21:57:36 +0530

    Added solution - LeetHub

---
+++ 700953-welcome-aboard-python-GFG/700953-welcome-aboard-python.py
@@ -0,0 +1,7 @@
+# Function to Welcome the person
+def welcomeAboard(name):
+    print("Welcome",name,sep=" ")  # Your code here
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
