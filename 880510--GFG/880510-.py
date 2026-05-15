58c84f1059b2cfaed50c5e86682c1bf6895c7e50
Karthik K <157501524+karthik-7777777@users.noreply.github.com>
2026-05-15 21:29:23 +0530

    Added solution - LeetHub

---
+++ 880510--GFG/880510-.py
@@ -0,0 +1,21 @@
+#User function Template for python3
+a = input()
+b = input()
+separator = input()[0]
+
+########### Write your code below ###############
+
+# Print with space
+print(a,b)
+# Print without newline at the end
+print(a,b,end="")
+# Print with separator
+print(a+separator+b)
+# Print without space
+print(a+b)
+########### Write your code above ###############
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
