ac0d3f16e16dcce4804f769a3834145a35921e03
Karthik K <157501524+karthik-7777777@users.noreply.github.com>
2026-05-15 21:34:38 +0530

    Added solution - LeetHub

---
+++ 705516-calculator-GFG/705516-calculator.py
@@ -0,0 +1,15 @@
+class Solution:
+    def utility(self, a, b, opr):
+        # code here
+        if opr==1:
+            print(a+b)
+        elif opr==2:
+            print(a-b)
+        elif opr==3:
+            print(a*b)
+        else:
+            print("Invalid Input")
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
