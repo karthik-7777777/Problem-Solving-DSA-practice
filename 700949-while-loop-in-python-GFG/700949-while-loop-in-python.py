d2ecfef07c9693508a5b1fe2caf9b476a06bb363
Karthik K <157501524+karthik-7777777@users.noreply.github.com>
2026-05-15 21:39:58 +0530

    Added solution - LeetHub

---
+++ 700949-while-loop-in-python-GFG/700949-while-loop-in-python.py
@@ -0,0 +1,14 @@
+def printInDecreasing(x):
+    # code here
+    while (x >= 0):
+        print(x,end=" ")
+        # your statement below to print the number
+        # in decreasing order
+        # Note: use end=" " parameter with print to seperate numbers by space.
+        ##Output for testcases will automatically separated by a new line by the print() in driver code
+
+        x -= 1
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
