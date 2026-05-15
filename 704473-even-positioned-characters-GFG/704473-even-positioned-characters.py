4ace9682f6b19a54eacc7a3ebe782ce00f4042c8
Karthik K <157501524+karthik-7777777@users.noreply.github.com>
2026-05-15 21:40:54 +0530

    Added solution - LeetHub

---
+++ 704473-even-positioned-characters-GFG/704473-even-positioned-characters.py
@@ -0,0 +1,8 @@
+def utility(s):
+    for i in range(len(s)):
+        if i%2==0:
+            print(s[i],end="")
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
