79cfe1e88a3ad4f5bbcd0c3b060aca81d389841e
Karthik K <157501524+karthik-7777777@users.noreply.github.com>
2026-05-15 22:01:22 +0530

    Added solution - LeetHub

---
+++ 700959--GFG/700959-.py
@@ -0,0 +1,18 @@
+def trim(str):
+    return str.strip()
+
+
+def exists(str, x):
+    return str.find(x)
+
+def titleIt(str):
+    
+    return str.title()
+
+
+def casesSwap(str):
+    return str.upper()
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
