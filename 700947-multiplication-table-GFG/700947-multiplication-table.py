d7290e8e369d6785ffa0a747466420705af16e01
Karthik K <157501524+karthik-7777777@users.noreply.github.com>
2026-05-15 21:10:05 +0530

    Added solution - LeetHub

---
+++ 700947-multiplication-table-GFG/700947-multiplication-table.py
@@ -0,0 +1,8 @@
+n = int(input())
+for i in range(1,11):
+    print(n*i,end=" ")
+# code here
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
