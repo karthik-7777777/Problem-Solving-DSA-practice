0c571097fd54c7d72949154e4f05ace2a32e2c6e
Karthik K <157501524+karthik-7777777@users.noreply.github.com>
2026-05-15 21:55:24 +0530

    Added solution - LeetHub

---
+++ 701595--GFG/701595-.py
@@ -0,0 +1,11 @@
+#User function Template for python3
+
+class Solution:
+     def reverseString(self, s: str) -> str:
+        # code here
+        st="".join(reversed(s))
+        return st
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
