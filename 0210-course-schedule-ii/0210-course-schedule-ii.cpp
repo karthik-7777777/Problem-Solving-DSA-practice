88b2abbf75d1418e2250ed1f9a745ee8e173cf32
Karthik K <157501524+karthik-7777777@users.noreply.github.com>
2026-06-08 20:53:38 +0530

    Time: 86 ms (5.08%), Space: 19.3 MB (10.5%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>>adj(numCourses);
+        for(auto it:prerequisites){
+            adj[it[1]].push_back(it[0]);
+        }
+        for(int j=0;j<adj.size();j++){
+            cout << j << "-->";
+            for(int i=0;i<adj[j].size();i++){
+                cout << adj[j][i] <<" ";
+            }
+            cout << endl;
+        }
+        vector<int>indeg(numCourses,0);
+        for(auto it:adj){
+            for(int i=0;i<it.size();i++){
+                indeg[it[i]]++;
+            }
+        }
+        for(int i=0;i<indeg.size();i++){
+            cout << indeg[i] << " ";
+        }
+        queue<int>q;
+        for(int i=0;i<indeg.size();i++){
+            if(indeg[i]==0)q.push(i);
+        }
+        vector<int>ans;
+        while(q.size()){
+            int node=q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto it:adj[node]){
+                indeg[it]--;
+                if(indeg[it]==0)q.push(it);
+            }
+        }
+        if(ans.size()!=numCourses)return {};
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
