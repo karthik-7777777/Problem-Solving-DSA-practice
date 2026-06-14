8e169fd6cf9d8d1839f9e108f158e44aeb049268
Karthik K <157501524+karthik-7777777@users.noreply.github.com>
2026-06-14 19:39:21 +0530

    Time: 1 ms (74.14%), Space: 124.2 MB (85.28%) - LeetHub - Time: O(n), Space: O(1)

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -29,10 +29,13 @@ public:
         int ans=INT_MIN;
         while(x){
             ans=max(ans,head->val+x->val);
-            cout << head->val << " " << x->val << endl;
             head=head->next;
             x=x->next;
         }
         return ans;
     }
 };
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
