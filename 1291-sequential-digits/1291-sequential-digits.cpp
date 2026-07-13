22c86f1c9a60fbe81d6834196b097953396dc047
Karthik K <157501524+karthik-7777777@users.noreply.github.com>
2026-07-13 11:08:51 +0530

    Time: 0 ms (100%), Space: 8.2 MB (76.91%) - LeetHub - Time: O(9 * (log(high) + log(low))), Space: O(log(high) + log(low))

--- 1291-sequential-digits/1291-sequential-digits.cpp
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
 void fun(int i,int next,int low,int high,vector<int>&ans){
-    if(i>=low && i<=high){
-        ans.push_back(i);
-    }
+    if(i>=low && i<=high)ans.push_back(i);
     if(i>high || next>9)return;
     fun((i*10)+next,next+1,low,high,ans);
 }
     vector<int> sequentialDigits(int low, int high) {
         vector<int>ans;
-        for(int i=1;i<=9;i++){
-            fun(i,i+1,low,high,ans);
-        }
+        for(int i=1;i<=9;i++)fun(i,i+1,low,high,ans);
         sort(ans.begin(),ans.end());
         return ans;
     }
